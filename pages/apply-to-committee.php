d3ad57e77692baf23f75221a2e51a13ca8453f52
games-yall <145702819+games-yall@users.noreply.github.com>
2024-02-16 13:14:15 -0600

    Update apply-to-committee.php

--- pages/apply-to-committee.php
+++ pages/apply-to-committee.php
@@ -50,25 +50,25 @@ require(__DIR__ . "/../partials/html-head.php");
     
 <h1>Committees Roles</h1>
 <p>
-<h4>General Committee Member</h4>
+<h4>General Committee Members - Jonathan Levine</h4>
 This role is for NEW Indie Games committee members. General Committee Members will assist in person at meetups as well as offering support to committee coordinators in their tasks. Committee members will receive tasks from the committee secretary and chair.
 <br><br>
-<h4>Chair</h4>
+<h4>Chair - Coby West</h4>
 This role organizes and leads committee meetings. They write committee meeting agendas and help organize future Games Y’all meetups. They speak directly to MoHA Staff with the needs  and concerns of the committee. They are available during Games Y'all events to help the event run smoothly, working directly with game devs.
 <br><br>
-<h4>Deputy chair</h4>
+<h4>Deputy Chair - Jay Roff-Garcia</h4>
 This role assists the chair. They help organize our committee meeting agenda, they are a stand-in if the chair or MoHA staff is unavailable, they are available for games y'all events and help to coordinate the volunteers at said events as well as answer questions from attendees.
 <br><br>
-<h4>Archivist</h4>
+<h4>Archivist - Cat Musgrove</h4>
 This role involves gathering/ taking notes during meetings  and sending those notes to the appropriate people whether they be missing members of the committee, Jay  or Rachel. The role is also responsible for distributing information via Discord and Asana, including direct contact with other members and  creating event notifications and invites. They collaborate with the MoHA Digital Art Coordinator in assigning tasks to Committee Members via Asana.
 <br><br>
-<h4>Games Coordinator</h4>
+<h4>Games Coordinator - Maya Andow</h4>
 The Games Coordinator recruits submissions from BIPOC, LGBTQIA+, disabled, women, and non-binary game developers/artists, inform folks about our open calls and meetups, socialize with and support devs at the event, develop themes for meetups based on submissions, pitch guest curators for meetups/showcases. They organize the monthly voting for Games Y’all selections. All committee members vote on selections!
 <br><br>
-<h4>Communications Coordinator</h4>
+<h4>Communications Coordinator - Eriane Austria</h4>
 The communication coordinator is responsible for coordinating or performing documentation of events, writing social media copy, and posting on social media. They also help with Discord moderation. 
 <br><br>
-<h4>Membership Coordinator</h4>
+<h4>Membership Coordinator - Albert Mancias</h4>
 They welcome new members into the Discord and invite members to gatherings. They help with communications, Discord moderation, and in-person onboarding to newsletter, discord, etc at events. They also help onboard new committee members.
 </p>
       </div>
