31606dfbadb91468e54be7eca92ee0acab405a56
games-yall <145702819+games-yall@users.noreply.github.com>
2024-04-19 14:20:42 -0500

    April Games Update

--- pages/event-april-2024.php
+++ pages/event-april-2024.php
@@ -72,68 +72,208 @@ require(__DIR__ . "/../partials/html-head.php");
         </div>
     </div>
 
-  <!--Games at the event
+  <!--Games at the event-->
         <div>
           <h2 style="text-align: center;">Games This Month</h2>
           
-          
-<!--HA-L card--
+<!--Speedway Survival card-->
           <div class="row" style="background-color: #9BE9FF; border-radius: 10px; margin-bottom: 25px;">
             <div class="game-card-long">
               <div>
-                <img src="/img/Archived-img/February-2024/games/ha-l.png" alt="- A promotional image for Hal">
-                </div>  
+                <img src="/img/Archived-img/April-2024/games/speedway-survival.png" alt="- A promotional image for Speedway Survival"> 
+              </div>  
 
-              <!--Game Text--
+              <!--Game Text-->
               <div>
-                <h4>HA-L</h4>
-                <p>Help HA-L fool the humans by laughing at jokes you don't understand! Don't laugh too much or not enough, otherwise you'll be discovered!
+                <h4>Speedway Survival</h4>
+                <p>Speedway survival is a survival horror game that takes you to the darkest hour at UT. As Sola, you have to defeat shadow monsters wreaking havoc on UT's speedway with your magical flashlight. Fighting your way through Speedway, you have to rescue Sola's sister Luna.
                 <br><br>
-             	 Ethan (Audio Lead at FarBridge) and David (Software Engineer, Audio at Panic Button) are both industry professionals who enjoy participating in game james. They have worked together over the past six years on various projects from audio optimization on big titles to silly brainstorms.
-              	</p>
+		"Skyler Kweon - Game Designer, Arts and Entertainment Technologies Sophomore @ UT Austin
+		<br>
+		Rachel Cannon - Artist, Sustainability Studies Senior @ UT Austin
+		<br>
+		Sebastian Davoli - Artist, Mechanical Engineer at Quaise Energy, UT Austin ME Graduate
+		<br>
+		Kalem Edlin - Audio Engineer, UT Austin CS Graduate
+		</p>
                 <button class="button"  style="margin: 10px; background-color: #F2D335;">
-                  <a href="https://globalgamejam.org/games/2024/ha-l-1">Play It Here</a>
+                  <a href="https://sunchankweon.itch.io/speedway-survival">Play it Here</a>
                 </button>
 
               </div>
 
-              <!--Dev Text--
+              <!--Dev Text-->
               <div>
-                <h6>Developers: Ethan & David</h6>
+                <h6>Developer: Bloody Longhorns<h6> 
 
                 </div>
               </div>
             </div>
-          </div>          
+          </div>         
           
-          
-<!--Stan Derp card--
+<!--Money-Driven FishTank Game-->
           <div class="row" style="background-color: #F2D335; border-radius: 10px; margin-bottom: 25px;">
             <div class="game-card-long">
               <div>
-                <img src="/img/Archived-img/February-2024/games/stan-derp.jpg" alt="- A promotional image for Stan Derp"> 
+                <img src="/img/Archived-img/April-2024/games/money-drive-fish.png" alt="- A promotional image for Money-Driven FishTank Game"> 
               </div>  
 
-              <!--Game Text--
+              <!--Game Text-->
               <div>
-                <h4>Stan Derp</h4>
-                <p>In Stan Derp, the player must keep their balance on stage to not fall down while delivering their standup routine. Players must build setup meter in order to deliver punchlines that get the crowd going, but none of that will work if you don't breathe!
-                </p>
+                <h4>Money-Driven FishTank Game</h4>
+                <p>Money-Driven Fish Tank Game is a game I built and designed on the popular children’s platform, Roblox. I built the game on the Roblox platform after reading that, although mostly for children, strip clubs and sex clubs kept appearing, and the platform had to hire more moderators to get them deleted. This was a huge “scandal” for the platform. As much of my work investigates how sex work operates in the contemporary landscape, thematically and conceptually, Roblox was an interesting starting point for me.
+ <br><br>
+In the game, the idea is to collect as much currency as possible (which the avatar can collect in the form of stacks of hundreds and green rectangles), avoid a giant shark that can kill you, avoid tiny cute fish that can kill you, listen to various characters that deliver extremely cliche and strange fables, and get through a difficult golden/damask maze to your home space.       
+<br><br>
+Dahlia Bloomstone (b.1995) is a Puerto Rican/American artist and Hunter College MFA (NY, 2022) graduate with a BFA from Bard College (NY, 2018). She was a Teaching Assistant at Hunter College from 2020-2022 and a Visiting Artist/Professor at UTAustin in 2023. At present, she is an LMCC resident on Governor's Island and a New York Community Trust Van Lier Fellow at Theater Mitu. Dahlia lives and works in New York.
+</p>
                 <button class="button"  style="margin: 10px; background-color: #9BE9FF;">
-                  <a href="https://globalgamejam.org/games/2024/stan-derp-4">Play It Here</a>
+                  <a href="https://www.dahliabloomstone.com">Website</a>
                 </button>
 
               </div>
 
-              <!--Dev Text--
+              <!--Dev Text-->
               <div>
-                <h6>Developer: Clicky Clack<h6> 
+                <h6>Developer: Dahlia Bloomstone<h6> 
+                <div class="dev-social">
+                  <h6>@the_dancr_next_door</h6>
+                  <a href="https://www.instagram.com/the_dancr_next_door">
+                    <img src="/img/Social-buttons/instagram-button-alt.png" style="width: 64px; height: auto;">
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div> 
+          
+<!--Sunsear: Avian Combat card-->
+          <div class="row" style="background-color: #9BE9FF; border-radius: 10px; margin-bottom: 25px;">
+            <div class="game-card-long">
+              <div>
+                <img src="/img/Archived-img/April-2024/games/sunsear.png" alt="- A promotional image for Sunsear: Avian Combat"> 
+              </div>  
+
+              <!--Game Text-->
+              <div>
+                <h4>Sunsear: Avian Combat</h4>
+                <p>Sunsear: Avian Combat is a bullet heaven/roguelite shoot-em-up where you select the best upgrades to win the fight against legions of imperial ships!
+    <br> - Collect upgrades for extra power and defenses
+    <br> - Withstand foes for 7 minutes, and then take down the region’s Flagship
+<br><br>
+This is a solo project that is an update to the game I released in August 2021
+<br><br>
+VisionBreak is a solo enterprise run by Adrian Walker, a software engineer with over 10 years of experience. Before running a studio, he created games self-taught as hobby, using accessible engines like RPG Maker. His work varies from story based role-playing games inspired by long-dead toylines to action-packed adventures starring a fearless gunship-piloting bird. He specializes in back-end development and video game music composition. 
+</p>
+                <button class="button"  style="margin: 10px; background-color: #F2D335;">
+                  <a href="https://visionbreak.dev/">Play it Here</a>
+                </button>
+
+              </div>
+
+              <!--Dev Text-->
+              <div>
+                <h6>Developer: VisionBreak<h6> 
+  <div class="dev-social">
+                  <h6>@VisionBreakDev</h6>
+                  <a href="https://twitter.com/VisionBreakDev">
+                    <img src="/img/Social-buttons/twitter-button.png" style="width: 64px; height: auto;">
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>         
+          
+<!--Waiting Room Game-->
+          <div class="row" style="background-color: #F2D335; border-radius: 10px; margin-bottom: 25px;">
+            <div class="game-card-long">
+              <div>
+                <img src="/img/Archived-img/April-2024/games/waiting-room.png" alt="- A promotional image for Waiting Room"> 
+              </div>  
+
+              <!--Game Text-->
+              <div>
+                <h4>Waiting Room</h4>
+                <p>Waiting Room is an RPG Maker game made as a companion piece to an undergraduate thesis on Yume Nikki and RPG maker space/time. We consider it "temporarily finished" as
+                <br><br>
+                Elliot Bear Yu and Kendle Chloe White are game designers, looking to make funny little projects and occasionally games(?) that work (but mostly ones that don’t)
+                </p>
+                <button class="button"  style="margin: 10px; background-color: #9BE9FF;">
+                  <a href="https://elliotbyu.itch.io/waiting-room">Play it Here</a>
+                </button>
+
+              </div>
+
+              <!--Dev Text-->
+              <div>
+                <h6>Developer: Dream House<h6> 
                 </div>
               </div>
             </div>
           </div>     
 
+<!--Untitled (Dragon Warrior glitches) card-->
+          <div class="row" style="background-color: #9BE9FF; border-radius: 10px; margin-bottom: 25px;">
+            <div class="game-card-long">
+              <div>
+                <img src="/img/Archived-img/April-2024/games/nilson-carrol.png" alt="- A promotional image for Untitled (Dragon Warrior glitches)"> 
+              </div>  
+
+              <!--Game Text-->
+              <div>
+                <h4>Untitled (Dragon Warrior glitches)</h4>
+                <p>Untitled (Dragon Warrior glitches) is an evolving HTML hypertext collection of captured handmade hex glitches from the NES titles Dragon Warrior II, III, and IV paired with prose poems that explore game mechanics, narratives, and genre legacies, queering each through the glitch process. Themes questioned include heteronormativity, binary thinking, colonization, and the neoliberal values inherent in these and all games’ mechanics and goals. Not only are the games’ digital/physical boundaries broken and expanded, but the implied ones, too. The myth of the hero is shattered, the protagonist finally made vulnerable.
+                <br><br>
+				Nilson Carroll is the Assistant Curator and Preservation Specialist at Visual Studies Workshop in Rochester, NY. A barista-ROM hacker, Nilson explores 16mm expanded cinema, video projection performance, and makes queer video game installations. He is a champion and curator of anti-fascist, feminist, pro-affection games and art. He co-founded the Queer Games Bundle and the DIY queer art games collective swampbabes.
+				</p>
+                <button class="button"  style="margin: 10px; background-color: #F2D335;">
+                  <a href="https://homepageguest.wixsite.com/website-16">Play it Here</a>
+                </button>
+
+              </div>
+
+              <!--Dev Text-->
+              <div>
+                <h6>Artist: Nilson Carroll<h6> 
+                </div>
+              </div>
+            </div>
+          </div>         
           
+<!--Open Roads Game-->
+          <div class="row" style="background-color: #F2D335; border-radius: 10px; margin-bottom: 25px;">
+            <div class="game-card-long">
+              <div>
+                <img src="/img/Archived-img/April-2024/games/open-roads.jpeg" alt="- A promotional image for Open Roads"> 
+              </div>  
+
+              <!--Game Text-->
+              <div>
+                <h4>Open Roads</h4>
+                <p>Long-lost family secrets. Hints of a hidden fortune. And miles to go before they sleep. Tess Devine’s relationship with her mom has never been easy, but they’re about to set out together on a journey into the past that they’ll never forget.
+                <br><br>
+              "Established in 2016, Annapurna Interactive works with game creators from around the world, helping them create and release personal experiences for everyone. The company is a division of Annapurna Pictures.
+   				 <br><br>
+				Some notable games we've released include WHAT REMAINS OF EDITH FINCH, OUTER WILDS, NEON WHITE, and STRAY."
+				 </p>
+                <button class="button"  style="margin: 10px; background-color: #9BE9FF;">
+                  <a href="https://store.steampowered.com/app/1497360/Open_Roads/">Play it Here</a>
+                </button>
+
+              </div>
+
+              <!--Dev Text-->
+              <div>
+                <h6>Developer: Annapurna Interactive<h6> 
+                 <div class="dev-social">
+                  <h6>@openroadsgame</h6>
+                  <a href="https://twitter.com/openroadsgame">
+                    <img src="/img/Social-buttons/twitter-button.png" style="width: 64px; height: auto;">
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div> 
+                    
     <!--Event Images-->
     <!--Only use this section after an event to house images from the past
     <div class="row">
