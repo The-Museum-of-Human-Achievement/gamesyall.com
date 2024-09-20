4f78b1381b0c04b4184ee7af3ab7a47205b29731
cobyw <4885716+cobyw@users.noreply.github.com>
2024-09-20 15:54:41 -0500

    Updated volunteer link in header to point to fantastic arcade interest form for now

--- partials/header.php
+++ partials/header.php
@@ -33,7 +33,7 @@
                     </a>
                     <div class="dropdown-content">
                         <a href="/submit-a-game"> Submit a Game</a>
-                        <a href="/volunteer">Volunteer</a>
+                        <a href="https://forms.gle/Hyetd1B6MBBXw4wGA">Volunteer</a>
                         <a href="/apply-to-committee">Apply to Committee</a>
                     </div>
                 </li>
