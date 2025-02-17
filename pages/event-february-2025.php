ae9e24ee8979eb26433490c7939564443e233ba2
FrancoJRamos <f.ramos57.09.5@gmail.com>
2025-02-17 13:49:43 -0600

    February 2025 Pre-Event Second Pass
    Filling out the information for the game section of the Feb 2025 event page.
    Editing the CSS for the dev-social class to add the display: contents rule, allowing multiple social buttons/icons to be displayed horizontally in the dev section of a project card.
    Also adding a rule to the .dev-social a element that removes text-decoration. this removes a blue underline near the buttons

--- pages/event-february-2025.php
+++ pages/event-february-2025.php
@@ -144,17 +144,23 @@ require(__DIR__ . "/../partials/html-head.php");
         <div class="row" style="background-color: #F2D335; border-radius: 10px; margin-bottom: 25px;">
             <div class="game-card-long">
                 <!--Promo Image-->
-                <img src="/img/Archived-img/February-2025/homepages/ifeelsomuchpain.jpg" alt="A promotional image for i feel so much pain">
+                <div>
+                    <img src="/img/Archived-img/February-2025/homepages/ifeelsomuchpain.jpg" alt="A promotional image for i feel so much pain">
+                </div>
                 <!--Project Text-->
+                <div>
                 <h4>i feel so much shame</h4>
                 <p>i feel so much shame is a browser-based interactive narrative made from scanned Risograph prints. Inspired by Risograph animation techniques, it offers a meditation on shame and desire as told through interactive and animated buttons, hyperlinks, inputs, and background gradients made embodied and material, with each and every moment of the website existing in printed form. Through a laborious process of printing, scanning, and realigning over 700 images and making them interactive with HTML, CSS, and JavaScript, the making of the piece–in all of its effort, repetition, and imperfections–also becomes one of its core realizations.</p>
                 <!--Call to Action-->
                 <button class="button"  style="margin: 10px; background-color: #9BE9FF;">
-                    <a href="https://ifeelsomuchsha.me/">Explore</a>
+                    <a href="https://ifeelsomuchsha.me/" target="_blank">Explore</a>
                 </button>
+                </div>
+                
                 <!--Dev Text-->
+                <div>
                 <h6>Developer: Jackie Liu</h6>
-                <p>Jackie Liu is a new media artist and digital designer who makes playful, interactive narratives that fall in between web experiences, comics, and games. Often drawing upon her early experiences of the internet, her work explores new ways of relating with herself, others, and technology. You can find her at <a href="https://jackieis.online" target="_blank">https://jackieis.online</a></p>
+                <p>Jackie Liu is a new media artist and digital designer who makes playful, interactive narratives that fall in between web experiences, comics, and games. Often drawing upon her early experiences of the internet, her work explores new ways of relating with herself, others, and technology. You can find her at<a href="https://jackieis.online" target="_blank">https://jackieis.online</a></p>
                 <div class="dev-social">
                     <a target="_blank" href="https://www.instagram.com/jackieis.online/">
                         <img src="/img/Social-buttons/instagram-button.png" style="width: 64px; height: auto;" alt="A link to Jackie Liu's Instagram account.">
@@ -164,6 +170,8 @@ require(__DIR__ . "/../partials/html-head.php");
                     </a>
 
                 </div>
+                </div>
+                
             </div>
         </div>
 
@@ -171,15 +179,22 @@ require(__DIR__ . "/../partials/html-head.php");
         <div class="row" style="background-color: #9BE9FF; border-radius: 10px; margin-bottom: 25px;">
             <div class="game-card-long">
                 <!--Promo Image-->
+                <div>
                 <img src="/img/Archived-img/February-2025/homepages/AntoniaBustamante.jpg" alt="A promotional image for Antonia Bustamante">
+                </div>
+                
                 <!--Project Text-->
+                <div>
                 <h4>Antonia Bustamante</h4>
                 <p>During her Welcome to my Homepage residency, Antonia Bustamante collected websites that inspire her, ranging from net art, poetry, and tools, to archives, newsletters, and community resources. They are arranged as a series of unmarked doors, allowing for chance explorations, countering the algorithmically determined pathways of social media feeds and search engines. A sidebar also provides an indexed list of the links, for quick scanning and intentional browsing. Heading the page are poetic questions and concepts, in Spanish and English, that guide the curation of the project and invite you to ponder the gesture of a website that leads you outside of itself. The project beckons you to wander its halls, or hop to a destination from the directory - each bringing a small joy and a new destination on Antonia’s personal map of the web.</p>
                 <!--Call to Action-->
                 <button class="button"  style="margin: 10px; background-color: #F2D335;">
-                    <a href="https://anattolia.github.io/www/welcomeToMyHomepage">Explore</a>
+                    <a href="https://anattolia.github.io/www/welcomeToMyHomepage"target="_blank">Explore</a>
                 </button>
+                </div>
+                
                 <!--Dev Text-->
+                <div>
                 <h6>Developer: Antonia Bustamante</h6>
                 <p>Antonia Bustamante is a coder and sound engineer based in Bogotá (Colombia). Her creative practice focuses on a critical and practical understanding of the intersection of arts and technologies. She is interested in collective ways of working, listening practices, and experimental and local ways of using technologies. Currently, she is studying for an MA in Philosophy and works mixing live music, teaching, researching, and developing web projects.</p>
                 <div class="dev-social">
@@ -190,6 +205,8 @@ require(__DIR__ . "/../partials/html-head.php");
                         <img src="/img/Social-buttons/github-button.png" style="width: 64px; height: auto;" alt="A link to Antonia Bustamante's Github account.">
                     </a>
                 </div>
+                </div>
+                
             </div>
         </div>
 
@@ -201,25 +218,128 @@ require(__DIR__ . "/../partials/html-head.php");
         <div class="row" style="background-color: #F2D335; border-radius: 10px; margin-bottom: 25px;">
             <div class="game-card-long">
                 <!--Promo Image-->
+                <div>
                 <img src="/img/Archived-img/February-2025/games/dr1v3nwild.png" alt="A promotional image for Drivin' Wild">
+                </div>
+                
                 <!--Project Text-->
+                <div>
                 <h4>DR1v3N wILD</h4>
                 <p>Race through 10 stunning stages in Driven Wild, an OutRun-inspired arcade driving game that fits in a 13 kilobyte file and runs on your web browser! Featuring procedural graphics, retro 3d rendering, and fast-paced physics. Can you master driving and make it to the magic garden at the end of the road?</p>
                 <!--Call to Action-->
-                <button class="button"  style="margin: 10px; background-color: #F2D335;">
-                    <a href="https://killedbyapixel.itch.io/dr1v3n-wild">Play Now</a>
+                <button class="button"  style="margin: 10px; background-color: #9BE9FF;">
+                    <a href="https://killedbyapixel.itch.io/dr1v3n-wild"target="_blank">Play Now</a>
                 </button>
+                </div>
+                
                 <!--Dev Text-->
+                <div>
                 <h6>Developer: Frank Force Games</h6>
                 <div class ="dev-social">
                     <a target="_blank" href="https://x.com/KilledByAPixel">
                         <img src="/img/Social-buttons/twitter-button.png" style="width: 64px; height: auto;" alt="A link to the Frank Force X account.">
                     </a>
                 </div>
+                </div>
+                
             </div>
         </div>
 
         <!--Magicland card-->
+        <div class="row" style="background-color: #9BE9FF; border-radius: 10px; margin-bottom: 25px;">
+	        <div class="game-card-long">
+                <!--Promo Image-->
+                <div>
+                <img src="/img/Archived-img/February-2025/games/magicland.png" alt="A promotional image for Magic Land">
+                </div>
+                
+                <!--Project Text-->
+                <div>
+                <h4>Magicland</h4>
+                <p>Have you ever wanted to do anything you've ever wanted to do?<br><br>Yes? Then come visit Magicland! The #1 destination for any adventurers looking to master the amazing skill of magic.<br><br>Magicland is a 2D-3D puzzle adventure. The goal of the game is to explore your surroundings and solve puzzles to advance the story. Your main goal is to find the great wizards to learn more spells and become more powerful, then use those powers to unlock new areas to explore.</p>
+                <!--Call to Action-->
+                <button class="button"  style="margin: 10px; background-color: #F2D335;">
+                    <a href="https://store.steampowered.com/app/3137050/Magicland/"target="_blank">Try the Demo and Wishlist Now</a>
+                </button>
+                </div>
+                
+                <!--Dev Text-->
+                <div>
+                <h6>Developer: Grant Ross</h6>
+                <p>Hi, I'm Grant. I'm a 25 year-old game dev from Austin. I try to make games for fun that will also leave an impact on people. I also work professionally as a technical game designer.</p>
+                <div class="dev-social">
+                    <a target="_blank" href="https://bsky.app/profile/froggy-loops.bsky.social">
+                        <img src="/img/Social-buttons/bluesky-button.png" style="width: 64px; height: auto;" alt="A link to Grant Ross's Bluesky account.">
+                    </a>
+                </div>
+                </div>
+                
+            </div>
+        </div>
+
+        <!--The Devil's Lounge card-->
+        <div class="row" style="background-color: #F2D335; border-radius: 10px; margin-bottom: 25px;">
+            <div class="game-card-long">
+                <!--Promo Image-->
+                <div>
+                <img src="/img/Archived-img/February-2025/games/thedevilslounge.png" alt="A promotional image for The Devil's Lounge">
+                </div>
+                
+                <!--Project Text-->
+                <div>
+                <h4>The Devil's Lounge</h4>
+                <p>What is The Lounge you ask?<br><br>The Devil's Lounge is 2-player dating app horror game. A tarot adventure of love, horror and self-transformation, of the monster you are becoming.<br><br>This game is born of experiences navigating dating and hookup apps in our digital era such as Tinder, Grindr, Bumble, Sniffies, etc. It features potential themes of horror, gore, romance and violence. <br><br>But you are not afraid of such a fate, in fact, you dare to look at this horror in the eye, and take in all of its contents.</p>
+                <!--Call to Action-->
+                <button class="button"  style="margin: 10px; background-color: #9BE9FF;">
+                    <a href="https://www.kickstarter.com/projects/corazoninteractive/the-devils-lounge"target="_blank">Fund Here!</a>
+                </button>
+                </div>
+                
+                <!--Dev Text-->
+                <div>
+                <h6>Developer: Corazon Interactive</h6>
+                <p>Corazon Interactive is a Movement Artist, Digital Queer Storyteller and Game Designer. She is also the Creator of The Devil's Lounge!</p>
+                <div class="dev-social">
+                    <a target="_blank" href="https://bsky.app/profile/corazoninteractive.bsky.social">
+                        <img src="/img/Social-buttons/bluesky-button.png" style="width: 64px; height: auto;" alt="A link to the Corazon Interactive Bluesky account.">
+                    </a>
+                </div>
+                </div>
+                
+            </div>
+        </div>
+        <!--gemporium card-->
+        <div class="row" style="background-color: #9BE9FF; border-radius: 10px; margin-bottom: 25px;">
+            <div class="game-card-long">
+                <!--Promo Image-->
+                <div>
+                <img src="/img/Archived-img/February-2025/games/gemporium.png" alt="A promotional image for Gemporium">
+
+                </div>
+                <!--Project Text-->
+                <div>
+                <h4>Gemporium</h4>
+                <p>"Mine, Refine and Sell Gemstones of various rarities to eagerly awaiting customers in this brief demo of Gemporium, a mining simulation management game. Manage your time and personal finances as you play through 10 days in the life of an anxious mole starting a Gem Emporium!<br><br>A demo for Gemporium is OUT NOW ON STEAM and wishlists and reviews are greatly appreciated<3"</p>
+                <!--Call to Action-->
+                <button class="button"  style="margin: 10px; background-color: #F2D335;">
+                    <a href="https://store.steampowered.com/app/3466550/Gemporium_Demo/?curator_clanid=45228107"target="_blank">Wishlist Now</a>
+                </button>
+                </div>
+                
+                <!--Dev Text-->
+                <div>
+                <h6>Developer: Merge Conflict Studio</h6>
+                <p>Merge Conflict Studio is an ATX based, indie game studio co-founded by 6 passionate developers who met at UT Austin. At Merge Conflict, we strive to make games that center on environmental awareness and marginalized perspectives, and we do not use generative AI in any capacity. We released our first game "Re:Fresh" in 2023 and have just released the demo for our second game "Gemporium" this year. If you are interested in reaching out, you can find us on our socials under @MergeConflictStudio :) </p>
+                <div class="dev-social">
+                    <!-- <a target="_blank" href="https://bsky.app/profile/froggy-loops.bsky.social">
+                        <img src="/img/Social-buttons/bluesky-button.png" style="width: 64px; height: auto;" alt="A link to the Grant Ross' Bluesky account.">
+                    </a> -->
+                </div>
+                </div>
+                
+            </div>
+        </div>
+    <!--End of Games at the event-->
     </div> 
     <!--Event Images-->
     <!--Only use this section after an event to house images from the past-->
