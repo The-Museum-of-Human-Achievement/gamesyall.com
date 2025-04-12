4045a44eef09283d354a81580d4aefbff8a8755e
Franco Ramos <f.ramos57.09.5@gmail.com>
2025-04-11 21:56:49 -0500

    April 2025 Event Updated
    Adding Game & Homepage Cards for:
    -a side view of a yellow car
    -blessings generator
    -cyberside picnic
    -ultraprocessor ribbon
    -holding patterns

--- pages/event-april-2025.php
+++ pages/event-april-2025.php
@@ -73,11 +73,195 @@ require(__DIR__ . "/../partials/html-head.php");
         </div>
     </div>
     
+    <!--Homepages at the event-->
+    <div>
+        <h2 style="text-align: center;">Homepages This Month</h2>
+        
+        <!--a side view of a yellow car Card-->
+        <div class="row" style="background-color: #F2D335; border-radius: 10px; margin-bottom: 25px;">
+            <div class="game-card-long">
+                <!--Promo Image-->
+                <div>
+                    <img src="/img/Archived-img/April-2025/homepages/a side view of a yellow car.jpg" alt="A promotional image for a side view of a yellow car. A pastel yellow Fiat Panda Parked outside of an apartment building.">
+                </div>
+                <!--Project Text-->
+                <div>
+                    <h4>a side view of a yellow car</h4>
+                    <p>a side view of a yellow car, is a tiny artistic web project I started in the context of the Welcome to My Homepage Residency. The aim is to experiment and toy with accessibility features such as alt text, audio transcription, but also low-tech media loaders designed for people with low-connectivity internet. The result is this intimate one-page website that features pictures of yellow cars alongside personal reflections on digital imagery in the age of self-driving cars. The website implements multiple accessibility features: an image loader that displays pixelated images by default with an added grainy aesthetic, a contextualized audio transcription of the page, and alt text for all images written in 5-7-5 short poem format.</p>
+                    <!--Call to Action-->
+                    <button class="button"  style="margin: 10px; background-color: #9BE9FF;">
+                        <a href="https://sideview.neocities.org/" target="_blank">Explore</a>
+                    </button>
+                </div>
+                
+                <!--Dev Text-->
+                <div>
+                <h6>Developer: Kaspar Ravel</h6>
+                <p>
+                    Kaspar Ravel (they/them) is an artist, teacher, and researcher in tactical new media. Their work draws from a tradition of art movements set in between symbolic activism and practical approaches (from subtle gestures to concrete actions). As a self-taught coder and DIY hacker, their intention is to demystify technology, which brings them to encourage low-tech cultures and digital literacy in educational contexts, such as workshops, tutorials and micro-edition. Today, they aspire to develop discursive and programmatic tools for resistance against the veil of big data and globalised surveillance. Their research takes the form of 'poelitical' experiments, merging poetic expression with political critique within on/offline artworks.
+                </p>
+                    <div class="dev-social">
+                        <a target="_blank" href="https://www.poelitics.org/">
+                            Website
+                        </a>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+
+        <!--blessings generator Card-->
+        <div class="row" style="background-color: #9BE9FF; border-radius: 10px; margin-bottom: 25px;">
+            <div class="game-card-long">
+                <!--Promo Image-->
+                <div>
+                    <img src="/img/Archived-img/April-2025/homepages/blessings generator.jpg" alt="A promotional image for blessings generator. 6 cups of tea arranged on a rectangular table. Surrounding the table is a mortar & pestle, incense burner, herbs, popcorn and a kettle.">
+                </div>
+                <!--Project Text-->
+                <div>
+                    <h4>ምርቃት (mirik’at) / blessings generator</h4>
+                    <p>
+                    The ምርቃት (mirik’at) / blessings generator is a collaborative web-based interactive work by Betelhem Makonnen, Mayet, and Mihret Kebede. Inspired by coffee ceremonies and the verbal art of well-wishing from their shared homeland of Ethiopia, the artists created this project to cultivate a new practice of collective care-taking for virtual space. 
+                    <br><br>
+                    The website allows visitors to receive blessings on topics ranging from world woes to future hopes, or to receive a customized blessing based on their specific concern. Sharing a wealth of knowledge about the practice of well-wishing, the project invites visitors to bring ምርቃት (mirik’at) into their lives to experience the sense of wellbeing and interconnectedness that come with rooting for each other and honoring interdependence.
+                    </p>
+                    <!--Call to Action-->
+                    <button class="button"  style="margin: 10px; background-color: #F2D335;">
+                        <a href="https://homepageguest.wixsite.com/blessings/" target="_blank">Explore</a>
+                    </button>
+                </div>
+                
+                <!--Dev Text-->
+                <div>
+                <h6>Developer: Betelhem Makonnen, Mayet, and Mihret Kebede</h6>
+                <p>
+                A native of Ethiopia, Betelhem Makonnen, researches perception, presence, and place within African diasporic consciousness. In addition to her studio practice she co-organizes Addis Video Art Festival.
+                <br><br>
+                Mayet explores the intersection of artificial intelligence, human connection, and creative expression. With a background in international relations, economics, and data science, she examines how technology shapes the way we learn, grow, and interact.
+                <br><br>
+                Mihret Kebede is a multidisciplinary artist, poet, and co-founder of Tobiya Poetic Jazz, Netsa Art Village Artists Collective, and Addis International Video Art Festival. Mihret's artistic journey spans local and international projects across cultures.
+                </p>
+                    <div class="dev-social">
+                        <a target="_blank" href="https://betelhemmakonnen.com/">
+                        https://betelhemmakonnen.com/
+                        </a>
+                        <a target="_blank" href="https://linktr.ee/mihretkebede">
+                        https://linktr.ee/mihretkebede
+                        </a>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+        <!--End of Homepages at the event-->
+    </div>
+    
+
     <!--Games at the event-->
     <div>
         <!-- TODO: fill out game section -->
-        <!-- <h2 style="text-align: center;">Games This Month</h2> -->
-       
+        <h2 style="text-align: center;">Games This Month</h2>
+        
+        <!--Cyberside Picnic Card-->
+        <div class="row" style="background-color: #F2D335; border-radius: 10px; margin-bottom: 25px;">
+            <div class="game-card-long">
+                <!--Promo Image-->
+                <div>
+                    <img src="/img/Archived-img/April-2025/games/Cyberside Picnic.jpg" alt="A promotional image for Cyberside Picnic. In the middle of a field during nightime, a computer window displays a website and is the only source of light in the picture.">
+                </div>
+                <!--Project Text-->
+                <div>
+                    <h4>Cyberside Picnic</h4>
+                    <p>
+                    Cyberside Picnic is a eulogy for the cyberian commonplace that just eluded us.  A love letter to a lost future of alternative video games, delete and overwrite.
+                    </p>
+                    <!--Call to Action-->
+                    <button class="button"  style="margin: 10px; background-color: #9BE9FF;">
+                        <a href="https://cathoderadiator.itch.io/cyberside-picnic" target="_blank">Play Now</a>
+                    </button>
+                </div>
+                
+                <!--Dev Text-->
+                <div>
+                <h6>Developer: Michael Luo</h6>
+                <p>
+                    Michael Luo is a Los Angeles-based media artist, game maker, and educator who explores interactivity, digital/social culture, and experimental media. He holds an MFA in Design Media Arts from UCLA, where he also completed his BA. Luo’s work has been exhibited internationally, including at SITE Santa Fe, A MAZE. Festival (Berlin), Playtopia Festival (Cape Town), and the Independent Games Festival. He is currently a Visiting Assistant Professor at Occidental College, where he teaches courses on game design and media arts.
+                </p>
+                    <div class="dev-social">
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+
+        <!--Ultraprocessor Ribbon Card-->
+        <div class="row" style="background-color: #9BE9FF; border-radius: 10px; margin-bottom: 25px;">
+            <div class="game-card-long">
+                <!--Promo Image-->
+                <div>
+                    <img src="/img/Archived-img/April-2025/games/Ultraprocessor Ribbon.jpg" alt="A promotional image for Ultraprocessor Ribbon. Two characters are displayed in the middle of the image: one looking to the left with a smile, her hair braided into pigtails with a CD emerging from her head. The other looks off to the right with a stern expression: there is a cpu attached to her chest and ribbon cables coming out and connecting back into her shoulders. ">
+                </div>
+                <!--Project Text-->
+                <div>
+                    <h4>Ultraprocessor Ribbon</h4>
+                    <p>
+                    Half brick-breaker action! Half erotic visual novel! Follow the story of Stella and her computer-turned-android Ribbon as they fight to make old computers legal again in the year 2673.
+                    </p>
+                    <!--Call to Action-->
+                    <button class="button"  style="margin: 10px; background-color: #F2D335;">
+                        <a href="https://demuresoft.xyz/ribbon/ribbon.php" target="_blank">Learn More</a>
+                    </button>
+                </div>
+                
+                <!--Dev Text-->
+                <div>
+                <h6>Developer: DemureSoft</h6>
+                <p>
+                DemureSoft is a doujinsoft circle based in Austin, TX, formed by Jennifer Raye to reject the bleak and cynical outlooks of making independent video games. We're carving a new path, one which we hope will inspire others to never lose sight of their passions, and to shout them brilliantly from the rooftops. We strive to push the agendas of bishoujo, eroge, and doujin in America. We will not rest until our city looks like Akihabara.
+                </p>
+                    <div class="dev-social">
+                        <a target="_blank" href="https://www.patreon.com/c/DemureSoft">
+                        https://www.patreon.com/c/DemureSoft
+                        </a>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+
+        <!--Holding Patterns Card-->
+        <div class="row" style="background-color: #F2D335; border-radius: 10px; margin-bottom: 25px;">
+            <div class="game-card-long">
+                <!--Promo Image-->
+                <div>
+                    <img src="/img/Archived-img/April-2025/games/Holding Patterns.jpg" alt="A promotional image for Holding Patterns. A Wooden puzzle block assembled but with clear markings denoting that it comes apart at various angles.">
+                </div>
+                <!--Project Text-->
+                <div>
+                    <h4>Holding Patterns</h4>
+                    <p>
+                    Holding Patterns are three dimensional wooden puzzles created from off-cuts from artist Michael Muelhaupt’s work, originally created for a group show “fitting” at Northern-Southern with Rachael Starbuck and Michael Muelhaupt. The nesting shapes became a metaphor for how social relationships define ourselves and create lasting impressions - community and culture build upon ourselves. 
+                    </p>
+                    <!--Call to Action-->
+                </div>
+                
+                <!--Dev Text-->
+                <div>
+                <h6>Developer: Jesse Cline</h6>
+                <p>
+                Jesse Cline is a designer, artist, and educator interested in contemporary and experimental methods of representation and production. 
+                </p>
+                    <div class="dev-social">
+                    
+                    
+                    <a target="_blank" href="https://www.instagram.com/superpattern">
+                        <img src="/img/Social-buttons/instagram-button.png" style="width: 64px; height: auto;" alt="A link to Jesse Cline's Instagram account.">
+                    </a>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
     <!--End of Games at the event-->
     </div> 
     <!--Event Images-->
